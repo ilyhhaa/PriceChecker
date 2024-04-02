21ec194d3d0a93a2aa8f9c831286be1bec8e3f8e
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-04-02 09:13:54 +0300

    "MVP Form is work Next to make tables working. "

--- PriceChecker/Form1.Designer.cs
+++ PriceChecker/Form1.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtName = new TextBox();
+            txtUrl = new TextBox();
+            textSelector = new TextBox();
+            URL = new Label();
+            label3 = new Label();
+            listTrackers = new ListView();
+            hdrName = new ColumnHeader();
+            hdrPrev = new ColumnHeader();
+            hdrCurrent = new ColumnHeader();
+            hdrLastChecked = new ColumnHeader();
+            hdrLastChanged = new ColumnHeader();
+            button1 = new Button();
+            lblTest = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Name:";
+            label1.Click += label1_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(116, 41);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(212, 23);
+            txtName.TabIndex = 1;
+            // 
+            // txtUrl
+            // 
+            txtUrl.Location = new Point(116, 86);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(212, 23);
+            txtUrl.TabIndex = 2;
+            // 
+            // textSelector
+            // 
+            textSelector.Location = new Point(116, 130);
+            textSelector.Name = "textSelector";
+            textSelector.Size = new Size(212, 23);
+            textSelector.TabIndex = 3;
+            // 
+            // URL
+            // 
+            URL.AutoSize = true;
+            URL.Location = new Point(71, 89);
+            URL.Name = "URL";
+            URL.Size = new Size(31, 15);
+            URL.TabIndex = 4;
+            URL.Text = "URL:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(63, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Selector:";
+            // 
+            // listTrackers
+            // 
+            listTrackers.Columns.AddRange(new ColumnHeader[] { hdrName, hdrPrev, hdrCurrent, hdrLastChecked, hdrLastChanged });
+            listTrackers.Location = new Point(68, 209);
+            listTrackers.Name = "listTrackers";
+            listTrackers.Size = new Size(662, 187);
+            listTrackers.TabIndex = 6;
+            listTrackers.UseCompatibleStateImageBehavior = false;
+            listTrackers.View = View.Details;
+            // 
+            // hdrName
+            // 
+            hdrName.Text = "Name";
+            // 
+            // hdrPrev
+            // 
+            hdrPrev.Text = "Prev Price";
+            // 
+            // hdrCurrent
+            // 
+            hdrCurrent.Text = "Current Price";
+            // 
+            // hdrLastChecked
+            // 
+            hdrLastChecked.Text = "Last Checked";
+            // 
+            // hdrLastChanged
+            // 
+            hdrLastChanged.Text = "LastChanged";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(527, 86);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 34);
+            button1.TabIndex = 7;
+            button1.Text = "Test";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lblTest
+            // 
+            lblTest.AutoSize = true;
+            lblTest.Location = new Point(599, 49);
+            lblTest.Name = "lblTest";
+            lblTest.Size = new Size(17, 15);
+            lblTest.TabIndex = 8;
+            lblTest.Text = "__";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblTest);
+            Controls.Add(button1);
+            Controls.Add(listTrackers);
+            Controls.Add(label3);
+            Controls.Add(URL);
+            Controls.Add(textSelector);
+            Controls.Add(txtUrl);
+            Controls.Add(txtName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtName;
+        private TextBox txtUrl;
+        private TextBox textSelector;
+        private Label URL;
+        private Label label3;
+        private ListView listTrackers;
+        private ColumnHeader hdrName;
+        private ColumnHeader hdrPrev;
+        private ColumnHeader hdrCurrent;
+        private ColumnHeader hdrLastChecked;
+        private ColumnHeader hdrLastChanged;
+        private Button button1;
+        private Label lblTest;
     }
 }
--- PriceChecker/Form1.cs
+++ PriceChecker/Form1.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace PriceChecker
 {
     public partial class Form1 : Form
@@ -5,6 +8,53 @@ namespace PriceChecker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var opt = new ChromeOptions();
+
+                opt.AddArgument(@"user-data-dir=C:/Users/Administrator/AppData/Local/Google/Chrome/User Data");
+                opt.AddArgument("headless");
+
+
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+
+                service.SuppressInitialDiagnosticInformation = true;
+                service.HideCommandPromptWindow = true;
+                var driver = new ChromeDriver(service, opt);
+
+                driver.Manage().Window.Minimize();
+                driver.Url = txtUrl.Text;
+                //https://mobi-lux.by/kompyutery-i-periferiya/videokarty/sapphire/videokarta-sapphire-radeon-rx-6700-xt-12gb-gddr6-11306-02-20g
+                System.Threading.Thread.Sleep(2000);
+
+                var v = driver.FindElement(By.XPath(textSelector.Text));
+                ////p[@class='sku__price ']
+                var str = v.Text;
+                driver.Quit();
+
+                lblTest.Text = str;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+
+
         }
     }
 }
